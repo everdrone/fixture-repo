ff543966338b804019a4e8e7c203f8520ed88485
everdrone <giorgiotropiano@gmail.com>
2018-05-29 21:07:57 +0200

    added sshkeygen (finish)

--- fixture.rb
+++ fixture.rb
@@ -14,3 +14,9 @@ puts "hash:       #{hash}"
 decoded = Fixture::Password.decode(hash)
 
 puts "can decode? #{decoded == password}"
+
+keygen = Fixture::Keygen.new
+
+key = keygen.generate('some@mail.com', password)
+
+puts key.ssh_public_key
